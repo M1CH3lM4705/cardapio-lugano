3a62f3c17c6bdc95873b9cbc2d1bf21f5441b564
Michel Matos de Oliveira <michel.oliveira@cge.ro.gov.br>
2024-06-25 21:17:43 -0400

    Adicionar arquivos de projeto.

---
+++ src/CardapioLugano.Data/Persistence/AppwriteBase.cs
@@ -0,0 +1,15 @@
+﻿using Appwrite;
+using Appwrite.Services;
+
+namespace CardapioLugano.Data.Persistence;
+public class AppwriteBase : IAppwriteBase
+{
+    private readonly Client _client;
+    public AppwriteBase(Client client)
+    {
+        _client = client;
+
+        Databases = new Databases(_client);
+    }
+    public Databases Databases {  get; }
+}
---
+++ src/CardapioLugano.Data/Persistence/IAppwriteBase.cs
@@ -0,0 +1,7 @@
+﻿using Appwrite.Services;
+
+namespace CardapioLugano.Data.Persistence;
+internal interface IAppwriteBase
+{
+    Databases Databases { get; }
+}
